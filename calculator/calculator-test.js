a928ba3475b1edc0b8e3d7cfb5f87788d905c5b4
pixls <tha.pixls@gmail.com>
2021-12-13 11:14:37 -0500

    added test logic for interest calc

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,14 @@
 
 it('should calculate the monthly rate correctly', function () {
   // ...
+  expect(calculateMonthlyPayment({amount: 1000, years: 10, rate: 2})).toEqual("9.2");
+
 });
 
 
 it("should return a result with 2 decimal places", function() {
   // ..
+  expect(calculateMonthlyPayment({amount: 1000, years: 10, rate: 2.7}).length).toEqual(4);
 });
 
 /// etc

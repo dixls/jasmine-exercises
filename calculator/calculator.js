e7ff9102b48c27714a004d06a9c2599336e794a6
pixls <tha.pixls@gmail.com>
2021-12-13 10:25:45 -0500

    added math logic

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,20 +21,36 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  document.getElementById("loan-amount").value = 0;
+  document.getElementById("loan-years").value = 0;
+  document.getElementById("loan-rate").value = 0;
+
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  updateMonthly(calculateMonthlyPayment(getCurrentUIValues()));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
+
 function calculateMonthlyPayment(values) {
+  let P = values["amount"];
+  let n = 12*values["years"];
+  let i = values["rate"]/100/12;
+  // console.log(i);
+  let divisor = 1-Math.pow(1+i, -n);
+  // console.log(divisor);
+  // console.log(P*i);
+  return `${Math.floor(P*i/divisor*100)/100}`;
+  
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  document.getElementById("monthly-payment").innerText = `$${monthly}`
 }
